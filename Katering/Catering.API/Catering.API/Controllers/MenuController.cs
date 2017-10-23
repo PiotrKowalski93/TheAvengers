73e191ec0b99c3c6376a57ee09ff5c96e892339a
Piotr Kowalski <kowalskii1@op.pl>
2017-10-23 21:51:14 +0200

    add API and Web Project

---
+++ Katering/Catering.API/Catering.API/Controllers/MenuController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Catering.API.Models;
+using Newtonsoft.Json;
+
+namespace Catering.API.Controllers
+{
+    public class MenuController : ApiController
+    {
+        // GET: api/Menu
+        public string Get()
+        {
+            string menuJson;
+
+            using (CateringDatabaseEntities context = new CateringDatabaseEntities())
+            {
+                List<Dish>  menuItems = context.Dish.ToList();
+
+                menuJson = JsonConvert.SerializeObject(menuItems);
+            }
+            
+            return menuJson;
+        }
+
+        // GET: api/Menu/5
+        public string Get(int id)
+        {
+            return "value";
+
+            
+        }
+
+        // POST: api/Menu
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Menu/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Menu/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Katering/hankel-app/src/App.tsx
@@ -0,0 +1,91 @@
+import * as React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const logo = require('./photos/mainPhoto.jpeg');
+
+class App extends React.Component {
+  render() {
+    return (
+        <div className="App">
+          <img src={logo} className="Main-photo" alt="logo" />
+          <br/>
+          <br/>
+          <UserButtons/>
+          <Tabs/>
+        </div>    
+    );
+  }
+}
+
+class UserButtons extends React.Component{
+  render(){
+    return (
+      <div className="btn-group d-flex justify-content-end">      
+        <a className="btn btn-primary" type="submit">Zaloguj</a>
+        <a className="btn btn-primary" type="submit">Rejestracja</a>
+      </div>    
+    );
+  }
+}
+
+class Tabs extends React.Component{
+  render(){
+    return (
+      <div>
+        <ul className="nav nav-tabs" id="myTab" role="tablist">
+        <li className="nav-item">
+          <a className="nav-link active" id="about-tab" data-toggle="tab" href="#about" role="tab" aria-controls="about" aria-selected="true">O NAS</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+        </li>
+      </ul>
+      <div className="tab-content" id="myTabContent">
+        <div className="tab-pane fade show active" id="about" role="tabpanel" aria-labelledby="about-tab"><AboutTab/></div>
+        <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab"><ProfileTab/></div>
+        <div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab"><ContactTab/></div>
+      </div>
+      </div>
+    );
+  }
+}
+
+class AboutTab extends React.Component{
+  render(){
+    return(
+        <div className="card mb-3">
+          <div className="card-body">
+            <h4 className="card-title">Card title</h4>
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+    );
+  }
+}
+
+class ProfileTab extends React.Component{
+  render(){
+    return(
+      <div>
+      </div>
+    );
+  }
+}
+
+class ContactTab extends React.Component{
+  render(){
+    return(
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
